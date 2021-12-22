da842bcc470e41b557a8250424c094d033223347
Omar <omarashraff11@gmail.com>
2021-12-22 20:48:50 +0200

    Project Initialization

---
+++ CSEDConnections/backend/src/test/java/com/connect/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connect.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
