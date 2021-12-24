f502dc190ba954a728605e92eea1d0a8019028cc
moaz886 <moaznabil886@gmail.com>
2021-12-24 09:28:02 +0200

    complet

--- CSEDConnections/backend/src/main/java/com/connect/backend/BackendApplication.java
+++ CSEDConnections/backend/src/main/java/com/connect/backend/BackendApplication.java
@@ -8,12 +8,12 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 		Database d=new Database();
-		System.out.println(d.checkEmail("moaz1@gmail.com"));
-		/*
-		System.out.println(d.checkUser("moaz@gmail.com" , "123456"));
-		Graduate user=new Graduate("Moaz Nabil","M" , "0100000" ,"Hi" , "23-2-2000" , "moaznabil886@gmail.com" , "123456879" , "url","2023" );
-		user.setExperience(new Experience("companyName", "location", "startDate", "endDate", "position"));
+		//System.out.println(d.checkEmail("moaz1@gmail.com"));
+		
+		//System.out.println(d.checkUser("moaz1@gmail.com" , "123456"));
+		Graduate user=new Graduate("Moaz Nabil","M" , "0100000" ,"Hi" , "23-2-2000" , "omarShonshory@gmail.com" , "123456879" , "url","2023" );
+		user.addExperience(new Experience("companyName", "location", "startDate", "endDate", "position"));
 		System.out.println(d.insertGraduate(user));
-		*/
+		
 	}
 }
