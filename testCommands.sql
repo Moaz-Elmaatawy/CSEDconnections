1864e3e0261f56be15fbec7e349bfe1b45188113
moaz886 <moaznabil886@gmail.com>
2021-12-24 03:09:03 +0200

    Test sql commands

---
+++ testCommands.sql
@@ -0,0 +1,7 @@
+use csedconnections;
+insert into student values ("moaz1@gmail.com", "moaz" , "2000-02-23" , "M" , "01021695101" , "123456" ,"url" ,"Hi i am moaz","3","2023");
+insert into student values ("moaz2@gmail.com", "moaz" , "2000-02-23" , "M" , "01021695102" , "123456" ,"url" ,"Hi i am moaz","3","2023");
+insert into student values ("moaz3@gmail.com", "moaz" , "2000-02-23" , "M" , "01021695103" , "123456" ,"url" ,"Hi i am moaz","3","2023");
+insert into student values ("moa4z@gmail.com", "moaz" , "2000-02-23" , "M" , "01021695104" , "123456" ,"url" ,"Hi i am moaz","3","2023");
+insert into student(email) values ('abderahman@gmail.com');
+select * from student
