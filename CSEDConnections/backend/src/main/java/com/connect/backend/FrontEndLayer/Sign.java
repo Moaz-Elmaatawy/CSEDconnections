2d62544b3a0a95575bdc5e30cefb161ac8f76fd5
Omar Ashraf <53156581+OmarAshraf1@users.noreply.github.com>
2022-01-04 07:31:22 +0200

    Update Sign.java

--- CSEDConnections/backend/src/main/java/com/connect/backend/FrontEndLayer/Sign.java
+++ CSEDConnections/backend/src/main/java/com/connect/backend/FrontEndLayer/Sign.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*")
-@RequestMapping("api")
+//@RequestMapping("api")
 @RestController
 public class Sign {
 
