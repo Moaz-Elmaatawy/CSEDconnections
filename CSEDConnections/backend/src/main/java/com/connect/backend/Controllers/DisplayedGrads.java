e44386b7c3ecd5395171194407325f13f263b20d
AbdelrahmanElmeniawy <52868473+AbdelrahmanElmeniawy@users.noreply.github.com>
2022-01-04 07:44:15 +0200

    Display testing

--- CSEDConnections/backend/src/main/java/com/connect/backend/Controllers/DisplayedGrads.java
+++ CSEDConnections/backend/src/main/java/com/connect/backend/Controllers/DisplayedGrads.java
@@ -11,6 +11,22 @@ public class DisplayedGrads {
     img = i;
   }
 
+  public DisplayedGrads(String n, String e, String c, String l, String i) {
+    name = n;
+    email = e;
+    company = c;
+    location = l;
+    img = i;
+  }
+
   public DisplayedGrads() {
+  }
+
+  public boolean equalsTo(DisplayedGrads other) {
+    return name.equals(other.name)
+        && email.equals(other.email)
+        && company.equals(other.company)
+        && location.equals(other.location)
+        && img.equals(other.img);
   }
 }
--- CSEDConnections/backend/src/test/java/com/connect/backend/DisplayTest.java
+++ CSEDConnections/backend/src/test/java/com/connect/backend/DisplayTest.java
@@ -1,5 +1,8 @@
 package com.connect.backend;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import com.connect.backend.Controllers.DisplayController;
@@ -8,14 +11,130 @@ import com.connect.backend.Controllers.DisplayedGrads;
 import org.junit.jupiter.api.Test;
 
 public class DisplayTest {
-    boolean equals(List<DisplayedGrads> expected, List<DisplayedGrads> actual){
-        if (expected.size() != actual.size()) return false;
-        for()
+    boolean equals(List<DisplayedGrads> expected, List<DisplayedGrads> actual) {
+        if (expected.size() != actual.size())
+            return false;
+        for (int i = 0; i < expected.size(); i++) {
+            DisplayedGrads expectedGrad = expected.get(i);
+            DisplayedGrads actualGrad = actual.get(i);
+            if (!expectedGrad.equalsTo(actualGrad)) {
+                System.out.println(i);
+                return false;
+            }
+        }
+        return true;
     }
 
     @Test
-    void DisplayWithoutSearch() {
+    void DisplayWithoutSearchNULL() {
         DisplayController controller = new DisplayController();
-
+        List<DisplayedGrads> actual = controller.Display(null);
+        List<DisplayedGrads> expected = new ArrayList<>();
+        expected.add(new DisplayedGrads("Ahmed Samir", "Ahmed@gmail.com", "Meta", "London",
+                "https://scontent.fcai21-4.fna.fbcdn.net/v/t39.30808-6/212990641_2066485583492708_1606143835716174366_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=BILwHj7jftEAX_joLdk&_nc_ht=scontent.fcai21-4.fna&oh=00_AT8G5KrBWacD8R-8U4HqncBvC2DEEAW7BSEHZs1Ex-spxg&oe=61D80F25"));
+        expected.add(new DisplayedGrads("Abdelrahman Elmeniawy", "elmeniawy@gmail.com", "google", "ss", "ss"));
+        expected.add(new DisplayedGrads("Moaz Ahmed", "moaz@gmail.com", "google", "dss", "ssss"));
+        expected.add(new DisplayedGrads("neno google", "neno@gmail.com", "ggg", "egy", "sssxccv"));
+        assertTrue(equals(expected, actual));
     }
+
+    @Test
+    void DisplayWithoutSearchBlank0() {
+        DisplayController controller = new DisplayController();
+        List<DisplayedGrads> actual = controller.Display("");
+        List<DisplayedGrads> expected = new ArrayList<>();
+        expected.add(new DisplayedGrads("Ahmed Samir", "Ahmed@gmail.com", "Meta", "London",
+                "https://scontent.fcai21-4.fna.fbcdn.net/v/t39.30808-6/212990641_2066485583492708_1606143835716174366_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=BILwHj7jftEAX_joLdk&_nc_ht=scontent.fcai21-4.fna&oh=00_AT8G5KrBWacD8R-8U4HqncBvC2DEEAW7BSEHZs1Ex-spxg&oe=61D80F25"));
+        expected.add(new DisplayedGrads("Abdelrahman Elmeniawy", "elmeniawy@gmail.com", "google", "ss", "ss"));
+        expected.add(new DisplayedGrads("Moaz Ahmed", "moaz@gmail.com", "google", "dss", "ssss"));
+        expected.add(new DisplayedGrads("neno google", "neno@gmail.com", "ggg", "egy", "sssxccv"));
+        assertTrue(equals(expected, actual));
+    }
+
+    @Test
+    void DisplayWithoutSearchBlank1() {
+        DisplayController controller = new DisplayController();
+        List<DisplayedGrads> actual = controller.Display(" ");
+        List<DisplayedGrads> expected = new ArrayList<>();
+        expected.add(new DisplayedGrads("Ahmed Samir", "Ahmed@gmail.com", "Meta", "London",
+                "https://scontent.fcai21-4.fna.fbcdn.net/v/t39.30808-6/212990641_2066485583492708_1606143835716174366_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=BILwHj7jftEAX_joLdk&_nc_ht=scontent.fcai21-4.fna&oh=00_AT8G5KrBWacD8R-8U4HqncBvC2DEEAW7BSEHZs1Ex-spxg&oe=61D80F25"));
+        expected.add(new DisplayedGrads("Abdelrahman Elmeniawy", "elmeniawy@gmail.com", "google", "ss", "ss"));
+        expected.add(new DisplayedGrads("Moaz Ahmed", "moaz@gmail.com", "google", "dss", "ssss"));
+        expected.add(new DisplayedGrads("neno google", "neno@gmail.com", "ggg", "egy", "sssxccv"));
+        assertTrue(equals(expected, actual));
+    }
+
+    @Test
+    void DisplayWithoutSearchBlank2() {
+        DisplayController controller = new DisplayController();
+        List<DisplayedGrads> actual = controller.Display("      ");
+        List<DisplayedGrads> expected = new ArrayList<>();
+        expected.add(new DisplayedGrads("Ahmed Samir", "Ahmed@gmail.com", "Meta", "London",
+                "https://scontent.fcai21-4.fna.fbcdn.net/v/t39.30808-6/212990641_2066485583492708_1606143835716174366_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=BILwHj7jftEAX_joLdk&_nc_ht=scontent.fcai21-4.fna&oh=00_AT8G5KrBWacD8R-8U4HqncBvC2DEEAW7BSEHZs1Ex-spxg&oe=61D80F25"));
+        expected.add(new DisplayedGrads("Abdelrahman Elmeniawy", "elmeniawy@gmail.com", "google", "ss", "ss"));
+        expected.add(new DisplayedGrads("Moaz Ahmed", "moaz@gmail.com", "google", "dss", "ssss"));
+        expected.add(new DisplayedGrads("neno google", "neno@gmail.com", "ggg", "egy", "sssxccv"));
+        assertTrue(equals(expected, actual));
+    }
+
+    @Test
+    void DisplayWithSearch0() {
+        DisplayController controller = new DisplayController();
+        List<DisplayedGrads> actual = controller.Display("google");
+        List<DisplayedGrads> expected = new ArrayList<>();
+        expected.add(new DisplayedGrads("Abdelrahman Elmeniawy", "elmeniawy@gmail.com", "google", "ss", "ss"));
+        expected.add(new DisplayedGrads("Moaz Ahmed", "moaz@gmail.com", "google", "dss", "ssss"));
+        expected.add(new DisplayedGrads("neno google", "neno@gmail.com", "ggg", "egy", "sssxccv"));
+        assertTrue(equals(expected, actual));
+    }
+
+    @Test
+    void DisplayWithSearch1() {
+        DisplayController controller = new DisplayController();
+        List<DisplayedGrads> actual = controller.Display("Meta");
+        List<DisplayedGrads> expected = new ArrayList<>();
+        expected.add(new DisplayedGrads("Ahmed Samir", "Ahmed@gmail.com", "Meta", "London",
+                "https://scontent.fcai21-4.fna.fbcdn.net/v/t39.30808-6/212990641_2066485583492708_1606143835716174366_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=BILwHj7jftEAX_joLdk&_nc_ht=scontent.fcai21-4.fna&oh=00_AT8G5KrBWacD8R-8U4HqncBvC2DEEAW7BSEHZs1Ex-spxg&oe=61D80F25"));
+        expected.add(new DisplayedGrads("Abdelrahman Elmeniawy", "elmeniawy@gmail.com", "google", "ss", "ss"));
+        assertTrue(equals(expected, actual));
+    }
+
+    @Test
+    void DisplayWithSearch2() {
+        DisplayController controller = new DisplayController();
+        List<DisplayedGrads> actual = controller.Display("Ahmed");
+        List<DisplayedGrads> expected = new ArrayList<>();
+        expected.add(new DisplayedGrads("Ahmed Samir", "Ahmed@gmail.com", "Meta", "London",
+                "https://scontent.fcai21-4.fna.fbcdn.net/v/t39.30808-6/212990641_2066485583492708_1606143835716174366_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=BILwHj7jftEAX_joLdk&_nc_ht=scontent.fcai21-4.fna&oh=00_AT8G5KrBWacD8R-8U4HqncBvC2DEEAW7BSEHZs1Ex-spxg&oe=61D80F25"));
+        expected.add(new DisplayedGrads("Moaz Ahmed", "moaz@gmail.com", "google", "dss", "ssss"));
+        assertTrue(equals(expected, actual));
+    }
+
+    @Test
+    void DisplayWithSearch3() {
+        DisplayController controller = new DisplayController();
+        List<DisplayedGrads> actual = controller.Display("2023");
+        List<DisplayedGrads> expected = new ArrayList<>();
+        expected.add(new DisplayedGrads("Ahmed Samir", "Ahmed@gmail.com", "Meta", "London",
+                "https://scontent.fcai21-4.fna.fbcdn.net/v/t39.30808-6/212990641_2066485583492708_1606143835716174366_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=BILwHj7jftEAX_joLdk&_nc_ht=scontent.fcai21-4.fna&oh=00_AT8G5KrBWacD8R-8U4HqncBvC2DEEAW7BSEHZs1Ex-spxg&oe=61D80F25"));
+        expected.add(new DisplayedGrads("Moaz Ahmed", "moaz@gmail.com", "google", "dss", "ssss"));
+        assertTrue(equals(expected, actual));
+    }
+
+    @Test
+    void DisplayWithSearchNotExist0() {
+        DisplayController controller = new DisplayController();
+        List<DisplayedGrads> actual = controller.Display("2019");
+        List<DisplayedGrads> expected = new ArrayList<>();
+        assertTrue(equals(expected, actual));
+    }
+
+    @Test
+    void DisplayWithSearchNotExist2() {
+        DisplayController controller = new DisplayController();
+        List<DisplayedGrads> actual = controller.Display("Apple");
+        List<DisplayedGrads> expected = new ArrayList<>();
+        assertTrue(equals(expected, actual));
+    }
+
 }
